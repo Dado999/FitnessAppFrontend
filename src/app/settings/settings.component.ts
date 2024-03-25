c0617bb06a01b7ac4763e243e4a9ffe1528aa83a
damirfilipovic99@gmail.com <damirfilipovic999@gmail.com>
2024-03-26 00:11:48 +0100

    pc 25.3

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -2,11 +2,12 @@ import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormsModule} from "@angular/forms";
 import {ToolbarComponent} from "../toolbar/toolbar.component";
 import {AuthService} from "../auth.service";
-import {NgIf, NgStyle} from "@angular/common";
+import {Location, NgIf, NgStyle} from "@angular/common";
 import {MatTooltip} from "@angular/material/tooltip";
 import {MatDialogRef} from "@angular/material/dialog";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {catchError, Observable, ReplaySubject, throwError} from "rxjs";
+import {User} from "../models/user.model";
 
 @Component({
   selector: 'app-settings',
@@ -32,23 +33,25 @@ export class SettingsComponent {
 
   constructor(protected authService: AuthService,
               private dialogRef: MatDialogRef<SettingsComponent>,
-              private http: HttpClient) {
-  }
+              private http: HttpClient) { }
 
   saveChanges() {
     const formData = new FormData();
     formData.append('newAvatar', this.base64Output);
-    formData.append('user',JSON.stringify(this.authService.currentUser,['name','surname','username','password','avatar','email','admin','cityByIdCity']));
+    const userBlob = new Blob([JSON.stringify(this.authService.currentUser,['name','surname','username','password','avatar','email','admin','cityByIdCity'])], {type: "application/json"});
+    formData.append('user',userBlob);
     this.http.put<any>(`http://localhost:8080/users/`+ this.authService.currentUser?.iduser,formData).pipe(
       catchError(error => {
         console.log(error);
-        return throwError(error); // Forward the error to the subscriber
+        return throwError(error);
       })
     ).subscribe(response => {
         console.log(response);
       });
+    window.location.reload();
     this.dialogRef.close();
   }
+
   onFileSelected(event: Event) {
     if (event.target instanceof HTMLInputElement && event.target.files && event.target.files.length > 0) {
       this.handleFileInput(event.target.files[0]).subscribe(base64 => {
@@ -57,6 +60,7 @@ export class SettingsComponent {
       });
     }
   }
+
   handleFileInput(file: File):Observable<string> {
     const result = new ReplaySubject<string>(1);
     const reader = new FileReader();
